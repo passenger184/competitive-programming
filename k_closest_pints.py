10d670078a03992c217319243cad5a6676b084e3
passenger184 <97516238+passenger184@users.noreply.github.com>
2022-11-03 11:49:39 +0300

    Create k_closest_pints.py

---
+++ k_closest_pints.py
@@ -0,0 +1,5 @@
+import math
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points = sorted(points, key = lambda x:math.sqrt(pow(x[0], 2) + pow(x[1], 2)))
+        return points[:k]
